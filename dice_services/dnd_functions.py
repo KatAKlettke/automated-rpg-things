56280594808799f82b56b5bd113cee66e436ea69
Kat A. Klettke <programming@alexandraklettke.net>
2025-03-15 20:01:18 +0100

    - fixed docstring: added verbose-parameter to docstring for roll_dnd_stats and roll_dnd_ability_score - added clarification to verbose output

--- dice_services/dnd_functions.py
+++ dice_services/dnd_functions.py
@@ -10,6 +10,7 @@ import utilities as utils
 def roll_dnd_ability_score(verbose: bool = False) -> int:
     """
     Rolls a single D&D ability score
+    :param: verbose bool if set to True, prints all dice rolls as well as the discarded roll
     :return: int An ability score calculated from random dice rolls
     """
     dice_rolls: typing.List[int] = []
@@ -17,9 +18,13 @@ def roll_dnd_ability_score(verbose: bool = False) -> int:
         dice_rolls.append(next(utils.roll_die(6, 4)))
 
     if verbose:
-        print(dice_rolls)
+        print('Rolls:', dice_rolls)
 
     roll_to_discard: int = min(dice_rolls)
+
+    if verbose:
+        print('Discard lowest:', roll_to_discard)
+
     dice_rolls.remove(roll_to_discard)
 
     ability_score: int = sum(dice_rolls)
@@ -30,6 +35,7 @@ def roll_dnd_ability_score(verbose: bool = False) -> int:
 def roll_dnd_stats(verbose: bool = False) -> typing.Tuple[int, int, int, int, int, int]:
     """
     Creates a tuple of six D&D ability scores to use for creating a level 1 character
+    :param: verbose bool if set to True, prints all dice rolls as well as the discarded rolls
     :return: Tuple[int, int, int, int, int, int]
     """
     stat_roll: typing.List[int] = []
